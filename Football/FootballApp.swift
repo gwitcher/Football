9ca879cffcff3c54ccde598c319d1b3df329c600
Gabe Witcher <gabewitcher@gmail.com>
2025-03-03 12:34:08 -0800

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Gabe Witcher on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
